ad2decbfcae4b54e7142cc2d5ecb11729d1ac435
abhit-onelot <abhit@onelot.ph>
2026-02-17 19:18:20 +0530

    added s3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["wedsy.s3.amazonaws.com", "wedsy.s3.ap-south-1.amazonaws.com"],
+    domains: [
+      "wedsy.s3.amazonaws.com",
+      "wedsy.s3.ap-south-1.amazonaws.com",
+      "wedsy-images-prod.s3.amazonaws.com",
+      "wedsy-images-prod.s3.ap-south-1.amazonaws.com"
+    ],
   },
   async rewrites() {
     return [
